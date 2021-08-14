a61f0f3007500ce091dbf827478415d49406d2d3
davidgfb <davidgracia99@gmail.com>
2021-08-14 16:35:13 +0200

    Update CoordenadasEsfericasARectangulares.py

--- CoordenadasEsfericasARectangulares.py
+++ CoordenadasEsfericasARectangulares.py
@@ -1,11 +1,15 @@
 from math import sin, cos, radians
+from pygame         import init, Color
+from pygame.display import set_mode, update
+from pygame.gfxdraw import pixel
+from pygame.event   import get
 
 def ptoEsferaUnidad(alfa, beta, esX_Origen = True):
     '''
     OBJ: Convierte coordenadas esfericas a rectangulares.
     alfa, beta en grados sexagesimales -> x, y, z
     Formula obtenida a partir de las proyecciones vertical y
-    horizontal. Calcula el punto o vector d sobre la esfera unidad
+    horizontal. Calcula la normal del planoCam en la supEsfera unidad
 
     Dos modalidades: con x como origen de beta:
     x, y, z (p. def), con y: (y, x, z)
@@ -14,18 +18,39 @@ def ptoEsferaUnidad(alfa, beta, esX_Origen = True):
     x, y, z = cos(alfa) * cos(beta), sin(beta) * cos(alfa),\
               sin(alfa)
     x, y, z = round(x, 2), round(y, 2), round(z, 2)
-
     res = (x, y, z)
 
     if not esX_Origen:
        res = (y, x, z) 
 
     return res
-    
-alfa, beta, v = 0, 0, 5
+
+def imprimePtoCam(ptoCam):
+    posCam, rotCam = ptoCam
+    alfa, beta, gamma = rotCam
+
+    print("\nptoCam =", ptoCam, ", n =",\
+          ptoEsferaUnidad(alfa, beta))
+
+v, NEGRO, BLANCO, PANTALLA, ptoCam = 5,\
+            (0, 0, 0), (255, 255, 255), set_mode((300, 300)),\
+                        [[0, 0, 0], [0, 0, 0]] #CERO, cero = (0, 0, 0), [0, 0, 0]
+
+pixel(PANTALLA, 150, 150, BLANCO)
+init()
+update()
+imprimePtoCam(ptoCam)
 
 while True:
-    ent = input("\nqe/ad\n")
+    posCam, rotCam = ptoCam
+    alfa, beta, gamma = rotCam
+    
+    PANTALLA.fill(NEGRO)
+    
+    for e in get():
+        pass
+    
+    ent = input("\nqe/ad\n") #TODO: +- para ajustar v
 
     if ent in "qead": 
         if ent == "q":
@@ -40,7 +65,8 @@ while True:
         elif ent == "d":
             beta += v
 
-        print("\nalfa =", alfa, ", beta =", beta,\
-              ", d =", ptoEsferaUnidad(alfa, beta))
+        rotCam = [alfa, beta, gamma]
+        ptoCam = posCam, rotCam
 
+        imprimePtoCam(ptoCam)
         
