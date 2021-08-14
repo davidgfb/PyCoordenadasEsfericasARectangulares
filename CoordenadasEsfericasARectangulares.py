36caf9c59da501d35768dc89c304b93a7b3b7618
davidgfb <davidgracia99@gmail.com>
2021-08-14 15:47:44 +0200

    Update CoordenadasEsfericasARectangulares.py

--- CoordenadasEsfericasARectangulares.py
+++ CoordenadasEsfericasARectangulares.py
@@ -1,17 +1,46 @@
 from math import sin, cos, radians
 
-def ptoEsferaUnidad(alfa, beta):
+def ptoEsferaUnidad(alfa, beta, esX_Origen = True):
     '''
-    OBJ: convierte coordenadas esfericas a rectangulares.
+    OBJ: Convierte coordenadas esfericas a rectangulares.
     alfa, beta en grados sexagesimales -> x, y, z
-    formula obtenida a partir de las proyecciones vertical y horizontal
-    calcula el punto o vector d sobre la esfera unidad
+    Formula obtenida a partir de las proyecciones vertical y
+    horizontal. Calcula el punto o vector d sobre la esfera unidad
+
+    Dos modalidades: con x como origen de beta:
+    x, y, z (p. def), con y: (y, x, z)
     '''
     alfa, beta = radians(alfa), radians(beta)
-    x, y, z = cos(alfa) * cos(beta), sin(beta) * cos(alfa), sin(alfa)
+    x, y, z = cos(alfa) * cos(beta), sin(beta) * cos(alfa),\
+              sin(alfa)
+    x, y, z = round(x, 2), round(y, 2), round(z, 2)
 
-    return (round(x, 2), round(y, 2), round(z, 2))
+    res = (x, y, z)
+
+    if not esX_Origen:
+       res = (y, x, z) 
+
+    return res
     
-#PROBADOR
-print(ptoEsferaUnidad(alfa = 45, beta = 45))
+alfa, beta, v = 0, 0, 5
 
+while True:
+    ent = input("\nqe/ad\n")
+
+    if ent in "qead": 
+        if ent == "q":
+            alfa -= v
+
+        elif ent == "e":
+            alfa += v
+        
+        elif ent == "a":
+            beta -= v
+        
+        elif ent == "d":
+            beta += v
+
+        print("\nalfa =", alfa, ", beta =", beta,\
+              ", d =", ptoEsferaUnidad(alfa, beta))
+
+        
